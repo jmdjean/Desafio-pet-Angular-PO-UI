42c5dc3a8b50ff3ab4fed37f93a9f44defe5605b
Jean Medes Dinelli <jean.medes@sp01.local>
2020-12-17 15:52:19 -0300

    tela de pets em po ui

--- src/app/views/pet/pet-listagem/pet-listagem.component.ts
+++ src/app/views/pet/pet-listagem/pet-listagem.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PoDisclaimer, PoDisclaimerGroup, PoModalAction, PoModalComponent, PoPageAction, PoPageFilter, PoTableAction, PoTableColumn } from '@po-ui/ng-components';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { Dono } from 'src/app/models/dono';
 import { Pet } from 'src/app/models/pet';
@@ -20,11 +21,58 @@ export class PetListagemComponent implements OnInit {
     private spinner: NgxSpinnerService
   ) { }
 
+  public nomeFiltro: string;
+  public apelidoFiltro: string;
+  public racaFiltro: string;
+  public especieFiltro: string;
+  public nomeDonoFiltro: string;
+
   public nomeTela = "Pets";
   public pets: Array<Pet>;
   public donos: Array<Dono>;
-
   public filtro: string;
+
+  @ViewChild('advancedFilter', { static: true }) advancedFilter: PoModalComponent;
+
+  public readonly advancedFilterPrimaryAction: PoModalAction = {
+    action: this.onConfirmAdvancedFilter.bind(this),
+    label: 'Pesquisar'
+  };
+
+  public readonly advancedFilterSecondaryAction: PoModalAction = {
+    action: () => this.advancedFilter.close(),
+    label: 'Cancelar'
+  };
+
+  public columns: Array<PoTableColumn> = [
+    { property: 'name', label: 'Nome' },
+    { property: 'nickName', label: 'Apelido' },
+    { property: 'breed', label: 'Raça' },
+    { property: 'species', label: 'Especie' },
+    { property: 'donoName', label: 'Dono' }
+  ];
+
+  public readonly disclaimerGroup: PoDisclaimerGroup = {
+    change: this.onChangeDisclaimerGroup.bind(this),
+    disclaimers: [],
+    title: 'Filtros aplicados em nossa pesquisa'
+  };
+
+  public readonly filter: PoPageFilter = {
+    action: this.pesquisaRapida.bind(this),
+    advancedAction: this.openAdvancedFilter.bind(this),
+    placeholder: 'Pesquisa rápida ...'
+  };
+
+  public readonly actions: Array<PoPageAction> = [
+    { action: this.abrirModalAdicionar.bind(this), label: 'Cadastrar', icon: 'po-icon po-icon-plus' }
+  ];
+
+  public readonly tableActions: Array<PoTableAction> = [
+    { action: this.abriModalVisualizar.bind(this), label: 'Visualizar' },
+    { action: this.abriModalEditar.bind(this), label: 'Editar' },
+    { action: this.abriModalDeletar.bind(this), label: 'Remover', type: 'danger', separator: true }
+  ];
 
   ngOnInit(): void {
     this.modalService.resultConfirmDialog$.asObservable().subscribe(res => {
@@ -37,6 +85,68 @@ export class PetListagemComponent implements OnInit {
 
     this.buscarPets();
   }
+
+  //#region TABELA
+  private onConfirmAdvancedFilter() {
+    const addDisclaimers = (property: string, value: string, label: string) =>
+      value && this.disclaimerGroup.disclaimers.push({ property, value, label: `${label}: ${value}` });
+
+    this.disclaimerGroup.disclaimers = [];
+
+    addDisclaimers('name', this.nomeFiltro, 'Nome');
+    addDisclaimers('nickName', this.apelidoFiltro, 'Apelido');
+    addDisclaimers('breed', this.racaFiltro, 'Raça');
+    addDisclaimers('species', this.especieFiltro, 'Especie');
+    addDisclaimers('donoName', this.nomeDonoFiltro, 'Dono');
+
+    this.advancedFilter.close();
+  }
+
+  private openAdvancedFilter() {
+    this.advancedFilter.open();
+  }
+
+  private onChangeDisclaimerGroup(disclaimers: Array<PoDisclaimer>) {
+
+    disclaimers.forEach(disclaimer => {
+      console.log(disclaimer);
+    });
+
+    /*  if (!this.searchFilters.search) {
+       this.searchTerm = undefined;
+     }
+ 
+     this.loadData(this.searchFilters); */
+  }
+
+  public OrdenarTabela(event) {
+    let params: any = {};
+
+    if (event) {
+      params.order = `${event.type === 'ascending' ? '' : '-'}${event.column.property}`;
+    }
+
+    if (!params.order.includes('-')) {
+      this.donos = this.donos.sort(function (a, b) {
+        if (a[event.column.property] > b[event.column.property]) return -1;
+        if (a[event.column.property] < b[event.column.property]) return 1;
+
+        return 0;
+      });
+    }
+    else {
+      this.donos = this.donos.reverse();
+    }
+  }
+
+  private pesquisaRapida() {
+    this.disclaimerGroup.disclaimers = [{
+      label: 'Pesquisa rápida: ${this.filtro}',
+      property: 'search',
+      value: this.filtro
+    }];
+  }
+  //#endregion
 
   public abrirModalAdicionar() {
     this.modalService.abrirModalCadastroPet();
